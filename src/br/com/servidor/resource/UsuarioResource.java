be9eb32d32e4ea26231877a17b6737b8eda1e970
Neils01 <iurryy@yahoo.com.br>
2016-04-07 12:12:30 -0300

    servidor resde corretamente a validação de usuario

--- src/br/com/servidor/resource/UsuarioResource.java
+++ src/br/com/servidor/resource/UsuarioResource.java
@@ -6,6 +6,7 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 import br.com.servidor.controller.UsuarioController;
@@ -35,7 +36,6 @@ public class UsuarioResource {
 	@Path("/getUsuario/{email}/{senha}")
 	@Produces(MediaType.APPLICATION_JSON)
 	public Usuario getUsuario(@PathParam(value = "email") String email,@PathParam(value = "senha") String senha) {
-		System.out.println(email + "  " + senha);
 		Usuario usuario = usuarioController.getUsuario(email, senha);
 		usuario = usuarioReferenciaCircular.removendoReferenciasCirculares(usuario);
 		return usuario;
